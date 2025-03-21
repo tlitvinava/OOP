d5b61d98eb05a8a5c7a5f2c7a83641c5e12a5a27
Taisa Litvinava <t.litvinava@gmail.com>
2025-03-21 12:08:04 +0300

    Fixed triangle

--- canvas.py
+++ canvas.py
@@ -1,3 +1,153 @@
+# from figures.rectangle import Rectangle
+# from figures.circle import Circle
+# from figures.triangle import Triangle
+# from ansi_colors import ANSI_COLORS  
+
+# class Canvas:
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#         self.state = Canvas.init_state(width, height)
+#         self.figures = []
+#         self.next_id = 1
+#         self.symbol_color = ANSI_COLORS["reset"]  # Initial symbol color (reset)
+#         self.background_color = ANSI_COLORS["reset"]  # Initial background color (reset)
+#         self.undo_stack = []  # Stack for undo
+#         self.redo_stack = []  # Stack for redo
+
+#     @staticmethod
+#     def init_state(width, height):
+#         state = []
+#         for _ in range(height):
+#             state.append(list("=" * width))
+#         return state
+
+#     def save_state(self):
+#         self.undo_stack.append((self.state.copy(), self.figures.copy()))
+
+#     def undo(self):
+#         if self.undo_stack:
+#             self.redo_stack.append((self.state.copy(), self.figures.copy()))
+#             self.state, self.figures = self.undo_stack.pop()
+#             self.update_state()
+
+#     def redo(self):
+#         if self.redo_stack:
+#             self.undo_stack.append((self.state.copy(), self.figures.copy()))
+#             self.state, self.figures = self.redo_stack.pop()
+#             self.update_state()
+
+#     def draw_figure(self, figure):
+#         self.save_state()  # Save state before change
+#         figure.id = self.next_id
+#         self.figures.append(figure)
+#         self.update_state()
+
+#     def remove_figure(self, figure_id):
+#         self.save_state()  # Save state before change
+#         self.figures = [fig for fig in self.figures if fig.id != figure_id]
+#         self.update_state()
+
+#     def move_figure(self, figure_id, new_coordinates):
+#         self.save_state()  # Save state before change
+#         for figure in self.figures:
+#             if figure.id == figure_id:
+#                 figure.move(*new_coordinates)  # Assuming the figure has a move method
+#                 break
+#         self.update_state()
+
+#     def update_state(self):
+#         self.state = Canvas.init_state(self.width, self.height)
+#         for figure in self.figures:
+#             self.draw_figure_to_state(self.state, figure)
+
+#     def draw_figure_to_state(self, state, figure):
+#         if isinstance(figure, Rectangle):
+#             upper_row = state[figure.y1 - 1]
+#             for i in range(figure.x1 - 1, figure.x2):
+#                 upper_row[i] = self.symbol_color + '*'+ ANSI_COLORS["reset"]
+#             lower_row = state[figure.y2 - 1]
+#             for i in range(figure.x1 - 1, figure.x2):
+#                 lower_row[i] = self.symbol_color + '*'+ ANSI_COLORS["reset"]
+#             for i in range(figure.y1 - 1, figure.y2):
+#                 state[i][figure.x1 - 1] = self.symbol_color + '*'+ ANSI_COLORS["reset"]
+#                 state[i][figure.x2 - 1] = self.symbol_color + '*'+ ANSI_COLORS["reset"]
+        
+#         elif isinstance(figure, Circle):
+#             for y in range(figure.y - figure.radius, figure.y + figure.radius + 1):
+#                 for x in range(figure.x - figure.radius, figure.x + figure.radius + 1):
+#                     if (x - figure.x) ** 2 + (y - figure.y) ** 2 <= figure.radius ** 2:
+#                         if 0 <= x < self.width and 0 <= y < self.height:
+#                             state[y][x] = self.symbol_color + '*'+ ANSI_COLORS["reset"]
+
+#         elif isinstance(figure, Triangle):
+#             self.draw_line(figure.x1, figure.y1, figure.x2, figure.y2, '*', state)
+#             self.draw_line(figure.x2, figure.y2, figure.x3, figure.y3, '*', state)
+#             self.draw_line(figure.x3, figure.y3, figure.x1, figure.y1, '*', state)
+
+#     # def draw_line(self, x1, y1, x2, y2, char, state):
+#     #     dx = abs(x2 - x1)
+#     #     dy = abs(y2 - y1)
+#     #     sx = 1 if x1 < x2 else -1
+#     #     sy = 1 if y1 < y2 else -1
+#     #     err = dx - dy
+
+#     #     while True:
+#     #         if 0 <= x1 < self.width and 0 <= y1 < self.height:
+#     #             #state[y1][x1] = self.symbol_color + char
+#     #             state[int(y1)][int(x1)] = self.symbol_color + char
+#     #         if x1 == x2 and y1 == y2:
+#     #             break
+#     #         err2 = err * 2
+#     #         if err2 > -dy:
+#     #             err -= dy
+#     #             x1 += sx
+#     #         if err2 < dx:
+#     #             err += dx
+#     #             y1 += sy
+
+#     def draw_line(self, x1, y1, x2, y2, char, state):
+#     # Проверка на выход за границы
+#     if not (0 <= x1 < self.width and 0 <= y1 < self.height and 0 <= x2 < self.width and 0 <= y2 < self.height):
+        
+#         return  # Прекращаем выполнение, если координаты за пределами холста
+
+#     dx = abs(x2 - x1)
+#     dy = abs(y2 - y1)
+#     sx = 1 if x1 < x2 else -1
+#     sy = 1 if y1 < y2 else -1
+#     err = dx - dy
+
+#     while True:
+#         if 0 <= x1 < self.width and 0 <= y1 < self.height:
+#             state[int(y1)][int(x1)] = self.symbol_color + char  # Рисуем символ
+#         if x1 == x2 and y1 == y2:
+#             break
+#         err2 = err * 2
+#         if err2 > -dy:
+#             err -= dy
+#             x1 += sx
+#         if err2 < dx:
+#             err += dx
+#             y1 += sy
+
+#     def set_background_color(self, color):
+#         if color in ANSI_COLORS:
+#             self.background_color = ANSI_COLORS[color]
+#             self.display()  # Redraw the canvas with the new background color
+
+#     def set_symbol_color(self, color):
+#         if color in ANSI_COLORS:
+#             self.symbol_color = ANSI_COLORS[color]
+#             self.update_state()
+#             self.display()  # Redraw the canvas with the new symbol color
+
+#     def display(self):
+#         print(self.background_color, end="")  # Set the background color
+#         for row in self.state:
+#             print("".join(row))
+#         print(ANSI_COLORS["reset"], end="")  # Reset color
+
 from figures.rectangle import Rectangle
 from figures.circle import Circle
 from figures.triangle import Triangle
@@ -86,6 +236,10 @@ class Canvas:
             self.draw_line(figure.x3, figure.y3, figure.x1, figure.y1, '*', state)
 
     def draw_line(self, x1, y1, x2, y2, char, state):
+        # Проверка на выход за границы
+        if not (0 <= x1 < self.width and 0 <= y1 < self.height and 0 <= x2 < self.width and 0 <= y2 < self.height):
+            return  # Прекращаем выполнение, если координаты за пределами холста
+
         dx = abs(x2 - x1)
         dy = abs(y2 - y1)
         sx = 1 if x1 < x2 else -1
@@ -94,7 +248,7 @@ class Canvas:
 
         while True:
             if 0 <= x1 < self.width and 0 <= y1 < self.height:
-                state[y1][x1] = self.symbol_color + char + ANSI_COLORS["reset"]
+                state[int(y1)][int(x1)] = self.symbol_color + char  # Рисуем символ
             if x1 == x2 and y1 == y2:
                 break
             err2 = err * 2
@@ -113,6 +267,7 @@ class Canvas:
     def set_symbol_color(self, color):
         if color in ANSI_COLORS:
             self.symbol_color = ANSI_COLORS[color]
+            self.update_state()
             self.display()  # Redraw the canvas with the new symbol color
 
     def display(self):
--- main.py
+++ main.py
@@ -1,3 +1,253 @@
+# from canvas import Canvas
+# from figures.circle import Circle
+# from figures.rectangle import Rectangle
+# from figures.triangle import Triangle
+# from ansi_colors import ANSI_COLORS  
+
+
+# def get_int_input(prompt, min_value=None, max_value=None):
+#     while True:
+#         try:
+#             value = int(input(prompt))
+#             if (min_value is not None and value < min_value) or (max_value is not None and value > max_value):
+#                 print(f"Please enter a value between {min_value} and {max_value}.")
+#             else:
+#                 return value
+#         except ValueError:
+#             print("Invalid input. Please enter a valid integer.")
+
+# def main():
+#     canvas = None
+#     while True:
+#         print("\nMenu:")
+#         print('1. Display Canvas')
+#         print('2. Draw Shape')
+#         print('3. Remove Shape')
+#         print('4. Move Shape')
+#         print('5. Export Canvas to File')
+#         print('6. Import Canvas from File')
+#         print('7. Change Figure Color')
+#         print('8. Undo')  
+#         print('9. Redo')  
+#         choice = get_int_input("Choose an option (1-10): ", 1, 10)
+
+#         if choice == 1:
+#             if canvas is None:
+#                 print("You do not have a canvas. Create a new canvas:")
+#                 height = get_int_input("Enter height: ", 1)
+#                 width = get_int_input("Enter width: ", 1)
+#                 canvas = Canvas(width, height)
+
+#             print("Current canvas:")
+#             canvas.display()
+        
+#         # elif choice == 2:
+#         #     if canvas is None:
+#         #         print("You do not have a canvas. Create a new canvas:")
+#         #         height = get_int_input("Enter height: ", 1)
+#         #         width = get_int_input("Enter width: ", 1)
+#         #         canvas = Canvas(width, height)
+
+#         #     print("Which shape would you like to draw?")
+#         #     print("1. Rectangle")
+#         #     print("2. Circle")
+#         #     print("3. Triangle")
+#         #     shape_choice = get_int_input("Choose a shape (1-3): ", 1, 3)
+
+#         #     if shape_choice == 1:
+#         #         print("Enter the coordinates of the rectangle:")
+#         #         x1 = get_int_input("x1: ", 1, canvas.width)
+#         #         y1 = get_int_input("y1: ", 1, canvas.height)
+#         #         x2 = get_int_input("x2: ", 1, canvas.width)
+#         #         y2 = get_int_input("y2: ", 1, canvas.height)
+#         #         rectangle = Rectangle(x1, y1, x2, y2)
+#         #         canvas.draw_figure(rectangle)
+
+#         #     elif shape_choice == 2:
+#         #         print("Enter the coordinates of the circle:")
+#         #         x = get_int_input("Center x: ", 1, canvas.width)
+#         #         y = get_int_input("Center y: ", 1, canvas.height)
+#         #         radius = get_int_input("Radius: ", 1, min(canvas.width, canvas.height) // 2)
+#         #         circle = Circle(x, y, radius)
+#         #         canvas.draw_figure(circle)
+
+#         #     elif shape_choice == 3:
+#         #         print("Enter the coordinates of the triangle:")
+#         #         x1 = get_int_input("x1: ", 1, canvas.width)
+#         #         y1 = get_int_input("y1: ", 1, canvas.height)
+#         #         x2 = get_int_input("x2: ", 1, canvas.width)
+#         #         y2 = get_int_input("y2: ", 1, canvas.height)
+#         #         x3 = get_int_input("x3: ", 1, canvas.width)
+#         #         y3 = get_int_input("y3: ", 1, canvas.height)
+#         #         triangle = Triangle(x1, y1, x2, y2, x3, y3)
+#         #         canvas.draw_figure(triangle)
+
+#         #     print("Current canvas:")
+#         #     canvas.display()
+
+#         elif choice == 2:
+#             if canvas is None:
+#                 print("You do not have a canvas. Create a new canvas:")
+#         height = get_int_input("Enter height: ", 1)
+#         width = get_int_input("Enter width: ", 1)
+#         canvas = Canvas(width, height)
+
+#         print("Which shape would you like to draw?")
+#         print("1. Rectangle")
+#         print("2. Circle")
+#         print("3. Triangle")
+#         shape_choice = get_int_input("Choose a shape (1-3): ", 1, 3)
+
+#         if shape_choice == 1:
+#             print("Enter the coordinates of the rectangle:")
+#         x1 = get_int_input("x1: ", 1, canvas.width)
+#         y1 = get_int_input("y1: ", 1, canvas.height)
+#         x2 = get_int_input("x2: ", 1, canvas.width)
+#         y2 = get_int_input("y2: ", 1, canvas.height)
+#         rectangle = Rectangle(x1, y1, x2, y2)
+#         canvas.draw_figure(rectangle)
+
+#         elif shape_choice == 2:
+#             print("Enter the coordinates of the circle:")
+#             x = get_int_input("Center x: ", 1, canvas.width)
+#             y = get_int_input("Center y: ", 1, canvas.height)
+#             radius = get_int_input("Radius: ", 1, min(canvas.width, canvas.height) // 2)
+#             circle = Circle(x, y, radius)
+#             canvas.draw_figure(circle)
+
+#         elif shape_choice == 3:
+#             print("Enter the lengths of the sides of the triangle:")
+#             a = get_int_input("Length of side a: ", 1)
+#             b = get_int_input("Length of side b: ", 1)
+#             c = get_int_input("Length of side c: ", 1)
+
+#         print("Enter the coordinates of the center of the triangle:")
+#         center_x = get_int_input("Center x: ", 1, canvas.width)
+#         center_y = get_int_input("Center y: ", 1, canvas.height)
+
+#         # Создайте объект треугольника
+#         triangle = Triangle(center_x, center_y, a, b, c)
+#         canvas.draw_figure(triangle)
+
+#         print("Current canvas:")
+#         canvas.display()
+
+#         elif choice == 3:
+#             if canvas is None:
+#                 print("You do not have a canvas. Create a new canvas:")
+#                 height = get_int_input("Enter height: ", 1)
+#                 width = get_int_input("Enter width: ", 1)
+#                 canvas = Canvas(width, height)
+
+#             figure_id = get_int_input("Enter the ID of the shape to remove: ", 1)
+#             canvas.remove_figure(figure_id)
+
+#             print("Current canvas after removing the shape:")
+#             canvas.display()
+
+#         elif choice == 4:
+#             if canvas is None:
+#                 print("You do not have a canvas. Create a new canvas:")
+#                 height = get_int_input("Enter height: ", 1)
+#                 width = get_int_input("Enter width: ", 1)
+#                 canvas = Canvas(width, height)
+
+#             figure_id = get_int_input("Enter the ID of the shape to move: ", 1)
+#             for figure in canvas.figures:
+#                 if figure.id == figure_id:
+#                     if isinstance(figure, Rectangle):
+#                         new_x1 = get_int_input("Enter new x1: ", 1, canvas.width)
+#                         new_y1 = get_int_input("Enter new y1: ", 1, canvas.height)
+#                         new_x2 = get_int_input("Enter new x2: ", 1, canvas.width)
+#                         new_y2 = get_int_input("Enter new y2: ", 1, canvas.height)
+#                         canvas.move_figure(figure_id, (new_x1, new_y1, new_x2, new_y2))
+#                     elif isinstance(figure, Circle):
+#                         new_x = get_int_input("Enter new center x: ", 1, canvas.width)
+#                         new_y = get_int_input("Enter new center y: ", 1, canvas.height)
+#                         canvas.move_figure(figure_id, (new_x, new_y))
+#                     elif isinstance(figure, Triangle):
+#                         new_x1 = get_int_input("Enter new x1: ", 1, canvas.width)
+#                         new_y1 = get_int_input("Enter new y1: ", 1, canvas.height)
+#                         new_x2 = get_int_input("Enter new x2: ", 1, canvas.width)
+#                         new_y2 = get_int_input("Enter new y2: ", 1, canvas.height)
+#                         new_x3 = get_int_input("Enter new x3: ", 1, canvas.width)
+#                         new_y3 = get_int_input("Enter new y3: ", 1, canvas.height)
+#                         canvas.move_figure(figure_id, (new_x1, new_y1, new_x2, new_y2, new_x3, new_y3))
+#                     break
+#             else:
+#                 print("No shape found with that ID.")
+
+#             print("Current canvas after moving the shape:")
+#             canvas.display()
+
+#         elif choice == 5:
+#             if canvas is None:
+#                 print("You do not have a canvas. Create a new canvas:")
+#                 height = get_int_input("Enter height: ", 1)
+#                 width = get_int_input("Enter width: ", 1)
+#                 canvas = Canvas(width, height)
+
+#             filename = input("Enter the filename to export (e.g., canvas.txt): ")
+#             with open(filename, 'w') as file:
+#                 for row in canvas.state:
+#                     file.write(''.join(row) + '\n')
+#             print(f"Canvas exported to file {filename}.")
+
+#         elif choice == 6:
+#             filename = input("Enter the filename to import (e.g., canvas.txt): ")
+#             try:
+#                 with open(filename, 'r') as file:
+#                     lines = file.readlines()
+#                     if canvas is None:
+#                         height = len(lines)
+#                         width = max(len(line.strip()) for line in lines)
+#                         canvas = Canvas(width, height)
+
+#                     for i, line in enumerate(lines):
+#                         canvas.state[i] = list(line.strip())
+#                 print(f"Canvas imported from file {filename}.")
+#                 canvas.display()
+#             except FileNotFoundError:
+#                 print("File not found. Please check the filename.")
+#             except Exception as e:
+#                 print(f"An error occurred: {e}")
+
+#         elif choice == 7:
+#             print("Choose a symbol color:")
+#             for color in ANSI_COLORS.keys():
+#                 print(color)
+#             color_choice = input("Enter color: ")
+#             if color_choice in ANSI_COLORS:
+#                 canvas.set_symbol_color(color_choice)
+#                 print(f"Symbol color changed to {color_choice}.")
+#             else:
+#                 print("Invalid color choice.")
+
+
+#         elif choice == 8:
+#             print("Choose a background color:")
+#             for color in ANSI_COLORS.keys():
+#                 print(color)
+#             color_choice = input("Enter color: ")
+#             if color_choice in ANSI_COLORS:
+#                 canvas.set_background_color(color_choice)
+#                 print(f"Background color changed to {color_choice}.")
+#             else:
+#                 print("Invalid color choice.")
+
+#         elif choice == 9:
+#             canvas.undo()
+#             print("Last action undone.")
+#             canvas.display()
+
+#         elif choice == 10:
+#             canvas.redo()
+#             print("Returned to the last action.")
+#             canvas.display()
+
+# if __name__ == '__main__':
+#     main()
+
 from canvas import Canvas
 from figures.circle import Circle
 from figures.rectangle import Rectangle
@@ -26,10 +276,10 @@ def main():
         print('4. Move Shape')
         print('5. Export Canvas to File')
         print('6. Import Canvas from File')
-        print('7. Change Background Color')
-        print('8. Undo')  # New menu option
-        print('9. Redo')  # New menu option
-        choice = get_int_input("Choose an option (1-10): ", 1, 10)
+        print('7. Change Figure Color')
+        print('8. Undo')  
+        print('9. Redo')  
+        choice = get_int_input("Choose an option (1-9): ", 1, 9)
 
         if choice == 1:
             if canvas is None:
@@ -72,14 +322,29 @@ def main():
                 canvas.draw_figure(circle)
 
             elif shape_choice == 3:
-                print("Enter the coordinates of the triangle:")
+                # print("Enter the lengths of the sides of the triangle:")
+                # a = get_int_input("Length of side a: ", 1)
+                # b = get_int_input("Length of side b: ", 1)
+                # c = get_int_input("Length of side c: ", 1)
+
+                # print("Enter the coordinates of the center of the triangle:")
+                # center_x = get_int_input("Center x: ", 1, canvas.width)
+                # center_y = get_int_input("Center y: ", 1, canvas.height)
+
+                # # Создайте объект треугольника
+                # triangle = Triangle(center_x, center_y, a, b, c)
+                # canvas.draw_figure(triangle)
+
+                print("Enter the base and height of the right triangle:")
+                base = get_int_input("Base length: ", 1)
+                height = get_int_input("Height length: ", 1)
+
+                print("Enter the coordinates of the right angle vertex (bottom left corner):")
                 x1 = get_int_input("x1: ", 1, canvas.width)
                 y1 = get_int_input("y1: ", 1, canvas.height)
-                x2 = get_int_input("x2: ", 1, canvas.width)
-                y2 = get_int_input("y2: ", 1, canvas.height)
-                x3 = get_int_input("x3: ", 1, canvas.width)
-                y3 = get_int_input("y3: ", 1, canvas.height)
-                triangle = Triangle(x1, y1, x2, y2, x3, y3)
+
+                # Создайте объект прямоугольного треугольника
+                triangle = Triangle(x1, y1, base, height)
                 canvas.draw_figure(triangle)
 
             print("Current canvas:")
